be8a2baa0917f43c02ee361d9aa14a01171205e5
Pooja Amin <poojaamin@Poojas-MacBook-Pro.local>
2019-11-01 10:12:09 -0400

    backfiles

--- NPI_data_loop.py
+++ NPI_data_loop.py
@@ -7,9 +7,11 @@ Downloads all NBER npi data for years in yearlist and months in monthslist and v
 import wget
 import os
 import urllib.error
+import zipfile
 
 RAW_DATA_DIR = '/work/akilby/npi/raw'
 USE_VAR_LIST = ['taxcode', 'locline1','locline2','loccityname','locstatename','loczip']
+USE_MONTH_LIST= ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'Jul', 'Aug','Sept','Oct','Nov','Dec']
 
 if not os.path.isdir(RAW_DATA_DIR):
 	os.makedirs(RAW_DATA_DIR)
@@ -41,4 +43,17 @@ for variable in USE_VAR_LIST:
 					download_fail_list.append(download_path_stub)
 					print('Warning: data does not exist')
 
-
+for variable in USE_MONTH_LIST:
+	for year in range(2007, 2020):
+		download_path_stub2 = 'https://data.nber.org/npi/backfiles/NPPES_Data_Dissemination_%s_%s' % (variable, year)
+		try:
+			download_path3 = '%s.zip' % download_path_stub2
+			destination_path3 =  os.path.join(RAW_DATA_DIR, wget.detect_filename(download_path3))
+			if not os.path.isfile(destination_path3):
+				print('WGetting zip download_path3: %s' % download_path3)
+				wget.download(download_path3, out=RAW_DATA_DIR)
+			else:
+				print('File already downloaded to: %s' % destination_path3)
+		except(urllib.error.HTTPError):
+			download_fail_list.append(download_path_stub2)
+			print('Warning: data does not exist')
