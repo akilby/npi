a47d6d563a19f6b8db5b5bdfde43ec388d3f7b2e
Angela E Kilby <a.kilby@northeastern.edu>
2020-02-14 16:27:20 -0500

    aek updates to backfile processing'

--- MergeFiles.py
+++ MergeFiles.py
@@ -34,18 +34,85 @@ def concat_files_by_variable(variable, yearrange=range(2007, 2020)):
 
 
 for variable in USE_VAR_LIST:
-    df = concat_files_by_variable(variable)
+    df1 = concat_files_by_variable(variable)
+    df2 = concat_backfiles_by_variable(variable)
+    df = pd.concat([df1, df2], axis=0)
     df.to_csv('/work/akilby/npi/data/%s_nber.csv' % variable, index=False)
 
 
+USE_VAR_LIST_DICT = {'taxcode': ['Healthcare Provider Taxonomy Code_1',
+                                 'Healthcare Provider Taxonomy Code_2',
+                                 'Healthcare Provider Taxonomy Code_3',
+                                 'Healthcare Provider Taxonomy Code_4',
+                                 'Healthcare Provider Taxonomy Code_5',
+                                 'Healthcare Provider Taxonomy Code_6',
+                                 'Healthcare Provider Taxonomy Code_7',
+                                 'Healthcare Provider Taxonomy Code_8',
+                                 'Healthcare Provider Taxonomy Code_9',
+                                 'Healthcare Provider Taxonomy Code_10',
+                                 'Healthcare Provider Taxonomy Code_11',
+                                 'Healthcare Provider Taxonomy Code_12',
+                                 'Healthcare Provider Taxonomy Code_13',
+                                 'Healthcare Provider Taxonomy Code_14',
+                                 'Healthcare Provider Taxonomy Code_15'],
+                     'locline1': 'Provider First Line Business Practice Location Address',
+                     'locline2': 'Provider Second Line Business Practice Location Address',
+                     'loccityname': 'Provider Business Practice Location Address City Name',
+                     'locstatename': 'Provider Business Practice Location Address State Name',
+                     'loczip': 'Provider Business Practice Location Address Postal Code',
+                     'loctel': 'Provider Business Practice Location Address Telephone Number',
+                     'credential': 'Provider Credential Text',
+                     'credentialoth':  'Provider Other Credential Text',
+                     'fname': 'Provider First Name',
+                     'gender': 'Provider Gender Code',
+                     'lname': 'Provider Last Name (Legal Name)'}
+
+
+# FIND THE REST OF THESE
+['NPI',
+ 'orgname',
+ 'orgnameothcode',
+ 'fnameoth',
+ 'licnum',
+ 'licstate',
+ 'primtax',
+ 'Provider Enumeration Date',
+ 'Last Update Date']
+
+
 def concat_backfiles_by_variable(variable):
+    
+    backfile_list = ['/work/akilby/npi/raw/npidata_20050523-20081110.csv',
+                     '/work/akilby/npi/raw/npidata_20050523-20100111.csv',
+                     '/work/akilby/npi/raw/npidata_20050523-20100208.csv',
+                     '/work/akilby/npi/raw/npidata_20050523-20110314.csv']
+                     
+    use_variable = USE_VAR_LIST_DICT[variable]
+    
+    if isinstance(use_variable, list):
+        collist = ['NPI'] + use_variable
+        
+        df_main = pd.DataFrame()
+        for filename in backfile_list:
+            df = pd.read_csv(filename, usecols=collist, low_memory=False)
+            df = pd.wide_to_long(df, ["Healthcare Provider Taxonomy Code_"], i="NPI", j="seq").dropna()
+            df = df.reset_index().rename(columns={'NPI': 'npi', 'Healthcare Provider Taxonomy Code_': 'taxcode'})
+            filedate = filename.split('/')[-1].split('-')[1].replace('.csv', '')
+            year, month = filedate[:4], filedate[4:6]
+            df['month'] = pd.to_datetime('%s-%s' % (year, month))
+            df_main = pd.concat([df_main, df], axis=0)
+            
+    return df_main
 
-    backfile_list = ['npidata_20050523-20081110.csv',
-                     'npidata_20050523-20100111.csv',
-                     'npidata_20050523-20100208.csv',
-                     'npidata_20050523-20110314.csv']
 
-    Nov2008 = pd.read_csv('/work/akilby/npi/raw/npidata_20050523-20081110.csv',
-                          usecols=['NPI', 'Provider First Line Business Practice Location Address'])
-    Nov2008 = Nov2008.rename(columns={'NPI': 'npi', 'Provider First Line Business Practice Location Address': 'plocline1'})
-    Nov2008['month'] = pd.to_datetime('2008-11')
+query_dataset = 'taxcode'
+query_string = 'NPI=="390200000X"'
+output_variables = ['locline1', 'locline2', 'loccityname', 'locstatename', 'loczip']
+
+
+def query_npi(query_dataset, query_string, output_variables):
+    # 1. read in appropriate dataset
+    # 2. do the query on the data, get out all the NPI-months that match
+    # 3. extract from each dataset for the output varables, for each NPI
+    df.query(query_string)
+    pass
