b4c1cf6effe66c3cd5e813f6cf186478c88c492f
Angela E Kilby <a.kilby@northeastern.edu>
2020-02-29 02:06:48 -0500

    medicaid query

--- queries.py
+++ queries.py
@@ -65,22 +65,46 @@ def medicaid_providers(state=None,
     df = df1.merge(df2, how='left').merge(df3, how='left')
     assert df[['npi', 'month', 'seq']].drop_duplicates().shape[0] == df.shape[0]
 
-    if state:
-        medicaid = df.query('OTHPIDTY==5 and OTHPIDST=="%s"' % state)
-    else:
-        medicaid = df.query('OTHPIDTY==5')
-    for col in usecols:
-        dfm = pd.read_csv('/work/akilby/npi/data/%s.csv' % col)
-        medicaid = medicaid.merge(dfm, how='left')
-    
-    medicaid_short = medicaid[['npi', 'month']].drop_duplicates()
-    
-    df_lic = pd.read_csv('/work/akilby/npi/data/%s.csv' % 'PLICNUM')
-    df_lics = pd.read_csv('/work/akilby/npi/data/%s.csv' % 'PLICSTATE')
-    dfl = df_lics.merge(df_lic, how='outer')
-    medicaid_lic = medicaid_short.merge(dfl)
-    dft = pd.read_csv('/work/akilby/npi/data/ptaxcode.csv')
+    q = ('OTHPIDTY==5 and OTHPIDST=="%s"' % state) if state else 'OTHPIDTY==5'
+    medicaid_npis = df.query(q).npi.drop_duplicates()
+    othpid = df.merge(medicaid_npis)
 
-    medicaid_tax = medicaid_short.merge(dft)
+    list_of_dfs = []
+    for col in usecols:
+        print(col)
+        dfrc = pd.DataFrame()
+        dfr = pd.read_csv('/work/akilby/npi/data/%s.csv' % col, iterator=True,
+                          chunksize=1000000)
+        for dfrs in dfr:
+            dfrc = dfrc.append(dfrs[dfrs.npi.isin(medicaid_npis)])
+
+        list_of_dfs.append(dfrc)
+
+    usecols_dfs = list_of_dfs[0]
+    icol = ['npi', 'month']
+    new_columns = icol + (usecols_dfs.columns.drop(icol).tolist())
+    usecols_dfs = usecols_dfs[new_columns]
+    for d in list_of_dfs[1:]:
+        usecols_dfs = usecols_dfs.merge(d, how='outer')
+
+    df_lic = pd.DataFrame()
+    dfr = pd.read_csv('/work/akilby/npi/data/%s.csv' % 'PLICNUM', iterator=True,
+                      chunksize=1000000)
+    for dfrs in dfr:
+        df_lic = df_lic.append(dfrs[dfrs.npi.isin(medicaid_npis)])
+
+    df_lics = pd.DataFrame()
+    dfr = pd.read_csv('/work/akilby/npi/data/%s.csv' % 'PLICSTATE', iterator=True,
+                      chunksize=1000000)
+    for dfrs in dfr:
+        df_lics = df_lics.append(dfrs[dfrs.npi.isin(medicaid_npis)])
     
-    return medicaid, medicaid_tax, medicaid_lic
+    dfl = df_lics.merge(df_lic, how='outer')
+
+    df_tax = pd.DataFrame()
+    dfr = pd.read_csv('/work/akilby/npi/data/%s.csv' % 'PLICSTATE', iterator=True,
+                      chunksize=1000000)
+    for dfrs in dfr:
+        df_tax = df_tax.append(dfrs[dfrs.npi.isin(medicaid_npis)])
+
+    return othpid, usecols_dfs, dfl, df_tax
--- specific_queries/medicaid_IL.py
+++ specific_queries/medicaid_IL.py
@@ -4,7 +4,8 @@ sys.path.append('/home/akilby/Packages/npi')
 from queries import medicaid_providers
 
 print('starting')
-medicaid, medicaid_tax, medicaid_lic = medicaid_providers(state="IL")
-medicaid.to_csv('/work/akilby/npi/processed/medicaid_IL.csv')
-medicaid_tax.to_csv('/work/akilby/npi/processed/medicaid_IL_tax.csv')
-medicaid_lic.to_csv('/work/akilby/npi/processed/medicaid_IL_lic.csv')
+othpid, usecols_dfs, dfl, df_tax = medicaid_providers(state="IL")
+othpid.to_csv('/work/akilby/npi/processed/medicaid_IL_othpid.csv')
+usecols_dfs.to_csv('/work/akilby/npi/processed/medicaid_IL_pinfo.csv')
+dfl.to_csv('/work/akilby/npi/processed/medicaid_IL_lic.csv')
+df_tax.to_csv('/work/akilby/npi/processed/medicaid_IL_tax.csv')
