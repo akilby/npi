803f1e5782a677481a1b7fc360ecb8c91e83e34d
Angela E Kilby <a.kilby@northeastern.edu>
2020-03-17 17:49:24 -0400

    updating samhsa match

--- samhsa_match.py
+++ samhsa_match.py
@@ -49,56 +49,54 @@ class NPI(object):
         self.get_lname()
         self.fullnames = pd.merge(self.fname, self.lname, how='outer')
 
+# df = pd.merge(fname, lname, how='outer')
+# nonames = df[~df.npi.isin(df.dropna().npi)]
 
+# df = df.dropna()
+# df = df.reset_index(drop=True)
 
-df = pd.merge(fname, lname, how='outer')
-nonames = df[~df.npi.isin(df.dropna().npi)]
-
-df = df.dropna()
-df = df.reset_index(drop=True)
-
-states = locstatename[~locstatename.npi.isin(nonames.npi)]
-states = states.dropna()
-states = states.reset_index(drop=True)
+# states = locstatename[~locstatename.npi.isin(nonames.npi)]
+# states = states.dropna()
+# states = states.reset_index(drop=True)
 
 
 def _in(x, obj): return True if obj in x else False
 
 
-df = df[~df.plname.apply(lambda x: _in(x, '?'))].reset_index(drop=True)
-df = df[~df.pfname.apply(lambda x: _in(x, '?'))].reset_index(drop=True)
-df['plname'] = df['plname'].str.replace('+', '')
-df['plname'] = df['plname'].str.replace('[', '')
-df['plname'] = df['plname'].str.replace(';', '')
+# df = df[~df.plname.apply(lambda x: _in(x, '?'))].reset_index(drop=True)
+# df = df[~df.pfname.apply(lambda x: _in(x, '?'))].reset_index(drop=True)
+# df['plname'] = df['plname'].str.replace('+', '')
+# df['plname'] = df['plname'].str.replace('[', '')
+# df['plname'] = df['plname'].str.replace(';', '')
 
-df['pfname'] = df['pfname'].str.replace('+', '')
-df['pfname'] = df['pfname'].str.replace(';', '')
+# df['pfname'] = df['pfname'].str.replace('+', '')
+# df['pfname'] = df['pfname'].str.replace(';', '')
 
-df_parens = df[df.plname.apply(lambda x: _in(x, '('))]
-df = df[~df.plname.apply(lambda x: _in(x, '('))]
+# df_parens = df[df.plname.apply(lambda x: _in(x, '('))]
+# df = df[~df.plname.apply(lambda x: _in(x, '('))]
 
-df_parens['plname_alt'] = df_parens.plname.str.split('(').str[1].str.split(')').str[0]
-df_parens['plname_alt2'] = df_parens.apply(lambda x: x.plname.replace("(" + x.plname_alt + ")", '').strip(), axis=1)
-df_parens['plname_alt'] = df_parens.plname_alt.str.strip()
-df_parens = df_parens.set_index(['npi','pfname']).stack().reset_index().drop(columns=['level_2']).rename(columns={0:'plname'})
+# df_parens['plname_alt'] = df_parens.plname.str.split('(').str[1].str.split(')').str[0]
+# df_parens['plname_alt2'] = df_parens.apply(lambda x: x.plname.replace("(" + x.plname_alt + ")", '').strip(), axis=1)
+# df_parens['plname_alt'] = df_parens.plname_alt.str.strip()
+# df_parens = df_parens.set_index(['npi','pfname']).stack().reset_index().drop(columns=['level_2']).rename(columns={0:'plname'})
 
-df = df.append(df_parens).reset_index(drop=True)
+# df = df.append(df_parens).reset_index(drop=True)
 
-df_samhsa = unmatched[['NameFull', 'State', 'DateLastCertified']]
-df_samhsa = df_samhsa.drop_duplicates().reset_index(drop=True)
-df_samhsa['NameFull'] = df_samhsa.NameFull.str.upper()
-df_samhsa = df_samhsa.drop_duplicates().reset_index(drop=True)
+# df_samhsa = unmatched[['NameFull', 'State', 'DateLastCertified']]
+# df_samhsa = df_samhsa.drop_duplicates().reset_index(drop=True)
+# df_samhsa['NameFull'] = df_samhsa.NameFull.str.upper()
+# df_samhsa = df_samhsa.drop_duplicates().reset_index(drop=True)
 
 
-badl = ['(JR.)', '(M.D.)', '(RET.)', 'M., PH', 'M.D.,', 'M.D., PHD, DABA,',
-        'M .D.', 'M.D.', 'MD', 'NP', 'D.O.', 'PA', 'DO', '', '.D.', 'MPH',
-        'PH.D.', 'JR', 'M.D', 'PHD', 'P.A.', 'FNP', 'PA-C', 'M.P.H.',
-        'N.P.', 'III', 'SR.', 'D.', '.D', 'FASAM', 'JR.', 'MS',
-        'D', 'FAAFP', 'SR', 'D.O', 'CNS', 'F.A.S.A.M.', 'FNP-BC', 'P.C.',
-        'MBA', 'M.S.', 'PH.D', 'FACP', 'M.P.H', 'DANIEL', 'KHAN', 'CNM',
-        'NP-C', 'MR.', 'MDIV', 'FACEP', 'PLLC', 'M.A.', 'LLC', 'MR',
-        'DNP', 'PHD.', 'FNP-C', 'MD.', 'CNP', 'J.D.', 'IV', 'F.A.P.A.',
-        'DR.', 'M.D,', 'DABPM', 'M,D.', 'MS.']
+# badl = ['(JR.)', '(M.D.)', '(RET.)', 'M., PH', 'M.D.,', 'M.D., PHD, DABA,',
+#         'M .D.', 'M.D.', 'MD', 'NP', 'D.O.', 'PA', 'DO', '', '.D.', 'MPH',
+#         'PH.D.', 'JR', 'M.D', 'PHD', 'P.A.', 'FNP', 'PA-C', 'M.P.H.',
+#         'N.P.', 'III', 'SR.', 'D.', '.D', 'FASAM', 'JR.', 'MS',
+#         'D', 'FAAFP', 'SR', 'D.O', 'CNS', 'F.A.S.A.M.', 'FNP-BC', 'P.C.',
+#         'MBA', 'M.S.', 'PH.D', 'FACP', 'M.P.H', 'DANIEL', 'KHAN', 'CNM',
+#         'NP-C', 'MR.', 'MDIV', 'FACEP', 'PLLC', 'M.A.', 'LLC', 'MR',
+#         'DNP', 'PHD.', 'FNP-C', 'MD.', 'CNP', 'J.D.', 'IV', 'F.A.P.A.',
+#         'DR.', 'M.D,', 'DABPM', 'M,D.', 'MS.']
 
 
 def check_in(it, li):
@@ -110,8 +108,8 @@ def replace_end(namestr, stub):
             else namestr.strip())
 
 
-for b in badl:
-    df_samhsa['NameFull'] = df_samhsa.NameFull.apply(lambda x: replace_end(x, b))
+# for b in badl:
+#     df_samhsa['NameFull'] = df_samhsa.NameFull.apply(lambda x: replace_end(x, b))
 
 
 def npi_names_to_samhsa_matches(firstname, lastname, df_samhsa):
@@ -157,14 +155,14 @@ def npi_names_sahmsa_matches_statebatch(stateabbrev):
     return outcome_df, fn, ln
 
 
-outcome_df = pd.read_csv('/home/akilby/outcome_df_samhsa_match.csv')
-
-outcome_df.drop(columns=['Unnamed: 0'], inplace=True)
-
-merged = outcome_df.merge(locstatename, left_on=['npi', 'State'], right_on=['npi', 'plocstatename'])
-merged['diff'] = pd.to_datetime(merged['DateLastCertified']) - pd.to_datetime(merged['month'])
-merged = merged.sort_values(['NameFull', 'State', 'DateLastCertified', 'npi', 'diff'])
-merged['diff2'] = abs(merged['diff'])
-
-possibles = merged[['NameFull', 'State', 'DateLastCertified', 'npi', 'pfname', 'plname', 'diff2']].groupby(['NameFull', 'State', 'DateLastCertified', 'npi', 'pfname', 'plname']).min()
-possibles = possibles.merge(possibles.groupby(level=[0, 1, 2]).count(), left_index=True, right_index=True)
+# outcome_df = pd.read_csv('/home/akilby/outcome_df_samhsa_match.csv')
+# 
+# outcome_df.drop(columns=['Unnamed: 0'], inplace=True)
+# 
+# merged = outcome_df.merge(locstatename, left_on=['npi', 'State'], right_on=['npi', 'plocstatename'])
+# merged['diff'] = pd.to_datetime(merged['DateLastCertified']) - pd.to_datetime(merged['month'])
+# merged = merged.sort_values(['NameFull', 'State', 'DateLastCertified', 'npi', 'diff'])
+# merged['diff2'] = abs(merged['diff'])
+# 
+# possibles = merged[['NameFull', 'State', 'DateLastCertified', 'npi', 'pfname', 'plname', 'diff2']].groupby(['NameFull', 'State', 'DateLastCertified', 'npi', 'pfname', 'plname']).min()
+# possibles = possibles.merge(possibles.groupby(level=[0, 1, 2]).count(), left_index=True, right_index=True)
