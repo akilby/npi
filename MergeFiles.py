f05410713d95b164ffb02d47ff32265dd6f3a239
Angela E Kilby <a.kilby@northeastern.edu>
2020-03-21 16:19:56 -0400

    adding update functionality to MergeFiles.py

--- MergeFiles.py
+++ MergeFiles.py
@@ -202,11 +202,13 @@ def main():
         df.to_csv(os.path.join(DATA_DIR, '%s.csv' % variable),
                   index=False)
     else:
+        print('Updating:')
         df = pd.read_csv(os.path.join(DATA_DIR, '%s.csv' % variable))
         last_month = max(list(df.month.value_counts().index))
         searchlist = [x for x in nppes_month_list() if
                       (pd.to_datetime('%s-%s-01' % (x[0], x[1]))
                        > pd.to_datetime(last_month))]
+        print('months-', searchlist)
         df2 = process_variable(RAW_DATA_DIR, variable, searchlist)
         df = pd.concat([df, df2], axis=0)
         df.to_csv(os.path.join(DATA_DIR, '%s.csv' % variable),
