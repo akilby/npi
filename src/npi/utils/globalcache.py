e3b0fce5d8888d616c9f6a929f6be277ee169986
Angela E Kilby <a.kilby@northeastern.edu>
2020-04-22 18:23:16 -0400

    updating cacher

--- src/npi/utils/globalcache.py
+++ src/npi/utils/globalcache.py
@@ -3,5 +3,11 @@ from cache.config import config_path, load_config_package
 
 noisily, rerun = load_config_package(config_path(name='npi'))
 c = cache.Cache(noisily=noisily,
-                rerun=rerun,
-                configure='/work/akilby/npi/Cache/cache_config.txt')
+                rerun=rerun)
+
+
+# If cache installation is being used for multiple purposes, need
+# a separate configuration file
+# c = cache.Cache(noisily=noisily,
+#                 rerun=rerun,
+#                 configure='/work/akilby/npi/Cache/cache_config.txt')
