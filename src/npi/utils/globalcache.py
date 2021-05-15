753b9f9177866006a304cf0499592ff430554685
Angela E Kilby <a.kilby@northeastern.edu>
2021-05-15 01:42:31 -0400

    debugging print statements

--- src/npi/utils/globalcache.py
+++ src/npi/utils/globalcache.py
@@ -2,6 +2,8 @@ from tributaries import cache
 from tributaries.config import config_path, get_config_package
 
 name = __name__.split('.')[0]
+print(name)
+print(config_path(name=name))
 directory, noisily, rerun = get_config_package(config_path(name=name))
 
 # Directory: '/work/akilby/npi/Cache/Caches'
