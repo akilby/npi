0042fd87f8a4c2671e0cbb09433ea23a0474c1bb
Angela E Kilby <a.kilby@northeastern.edu>
2021-05-15 01:32:37 -0400

    fixing globalcache

--- src/npi/utils/globalcache.py
+++ src/npi/utils/globalcache.py
@@ -1,19 +1,14 @@
 from tributaries import cache
-from tributaries.config import config_path, load_config_package
+from tributaries.config import config_path, get_config_package
 
-print(__name__.split('.')[0])
-noisily, rerun = load_config_package(config_path(name='npi'))
+name = __name__.split('.')[0]
+directory, noisily, rerun = get_config_package(config_path(name=name))
 
+# Directory: '/work/akilby/npi/Cache/Caches'
 
-c = cache.Cache(configure={'directory': '/work/akilby/npi/Cache/Caches',
+c = cache.Cache(configure={'directory': directory,
                            'registry': [__name__],
                            'exclusion_list': []},
                 noisily=noisily,
                 rerun=rerun,
                 old_version=False)
-
-# If cache installation is being used for multiple purposes, need
-# a separate configuration file
-# c = cache.Cache(noisily=noisily,
-#                 rerun=rerun,
-#                 configure='/work/akilby/npi/Cache/cache_config.txt')
